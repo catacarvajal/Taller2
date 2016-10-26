f1641d9f1efe02351f0f7d7e161a6f47eab58612
Fernando <fernandorgs1089@gmail.com>
2016-10-26 16:44:18 -0300

    para el pozo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,24 +1,73 @@
 <?php 
 
 namespace App\Http\Controllers;
+use Khill\Lavacharts\Lavacharts;
+use Illuminate\Support\Facades\DB;
 use App\Month;
 use App\Rast;
 use App\Period;
 use App\Scenario;
 use App\Variable;
 
+
 class HomeController extends Controller {
 
 	public function index()
 	{
+		$consultaPunto = $this->consultaGrafico(1,1,1);//hacemos la consulta de 1 punto
+        $lava = $this->graficoPunto($consultaPunto);//hacemos el gráfico de ese punto con la consulta anterior
 		$periodo = Period::all();// traemos todos los periodos que existen en la bd
         $scenario = Scenario::all();
         $variable = Variable::all();
-		return view('index')->with('periodo',$periodo)->with('scenario',$scenario)->with('variable',$variable);
+        $datosTabla = $this->datosTabla(); //se obtienen los datos para llenar la tabla (mes, variable, promedio)
+		return view('index')->with('periodo',$periodo)->with('scenario',$scenario)->with('variable',$variable)->with('lava',$lava)->with('datosTabla',$datosTabla);
 	}
 
-	public function indexAmbos()
-	{
-		return view('indexAmbos');
-	}
+	
+	 public function graficoPunto($consulta)
+    {
+        
+        $lava = new Lavacharts; // See note below for Laravel
+                $grafico = $lava->DataTable();
+                $grafico->addStringColumn('Months of Year')
+                        ->addNumberColumn('T° mínima');
+                        for($i=0; $i<count($consulta); $i++)
+                        {
+                            $grafico->addRow([$consulta[$i]->name, $consulta[$i]->avg]);
+                        }
+                $lava->BarChart('grafico', $grafico, [
+                    'title' => 'Temperatura Mínima',
+                    'titleTextStyle' => [
+                        'color'    => '#eb6b2c',
+                        'fontSize' => 30
+                    ]
+                ]);
+
+        return $lava;
+    }
+    public function consultaGrafico($id_variable, $id_periodo, $id_escenario)
+    {
+       $consulta = DB::table('rast')
+        ->select(DB::raw('month.name,month.id,avg(ST_Value(ST_SetSRID(rast,4326), ST_SetSRID(ST_Point(-71.233333,-34.983333), 4326)))'))
+        ->join('register', 'register.id', '=', 'rast.id_register')
+        ->join('month', 'month.id', '=', 'register.id_month')
+        ->join('variable', 'variable.id', '=', 'register.id_variable')
+        ->where('register.id_period','=',$id_periodo)
+        ->orwhere('variable.id','=', $id_variable)
+        ->groupBy('month.id')
+        ->get();
+        return $consulta;
+    }
+    public function datosTabla()
+    {
+        $tabla = DB::table('rast')
+        ->select(DB::raw('month.name as mes, variable.name as variable, avg(ST_Value(rast, ST_SetSRID(ST_Point(-71.233333,-34.983333), 4326))) as promedio'))
+        ->join('register', 'register.id', '=', 'rast.id_register')
+        ->join('month', 'month.id', '=', 'register.id_month')
+        ->where('register.id_period', '=', '1')
+        ->join('variable', 'variable.id', '=', 'register.id_variable')
+        ->groupBy('month.id', 'variable.name')
+        ->get();
+        return ($tabla);
+    }  
 }
--- app/Http/Controllers/chartsController.php
+++ app/Http/Controllers/chartsController.php
@@ -24,8 +24,8 @@ class chartsController extends Controller
         $periodo = Period::all();// traemos todos los periodos que existen en la bd
         $scenario = Scenario::all();
         $variable = Variable::all();
-        $datosTabla = $this->datosTabla(1,1); //se obtienen los datos para llenar la tabla (mes, variable, promedio)
-        //dd(count($consultaPunto));
+        $datosTabla = $this->datosTabla(); //se obtienen los datos para llenar la tabla (mes, variable, promedio)
+
         return view('indexGrafico')-> with('lava',$lava)->with('periodo',$periodo)->with('scenario',$scenario)->with('variable',$variable)->with('datosTabla', $datosTabla);
       
     }
@@ -48,6 +48,7 @@ class chartsController extends Controller
                         'fontSize' => 30
                     ]
                 ]);
+
         return $lava;
     }
 
@@ -83,12 +84,11 @@ class chartsController extends Controller
         ->where('register.id_period','=',$id_periodo)
         ->orwhere('variable.id','=', $id_variable)
         ->groupBy('month.id')
-        ->orderBy('month.id')
         ->get();
         return $consulta;
     }
 
-    //recibo lo que tiene los labels //esto hay que hacerlo con ajax
+    //recibo lo que tiene los lavels //esto hay que hacerlo con ajax
     public function postGrafico(Request $request)
     {
         if ( $request->ajax())
@@ -96,24 +96,23 @@ class chartsController extends Controller
             $variable =$request->input('variable');
             $escenario =$request->input('escenario');
             $periodo =$request->input('periodo');
-           
-            return $variable;
+            $consultaPunto = $this->consultaGrafico($variable,$periodo,$escenario);
+            $lava = $this->DataTable($consultaPunto);
+            return $request;
             //return $periodo;
         }
         
     } 
 
-    public function datosTabla($id_variable, $id_periodo)
+    public function datosTabla()
     {
         $tabla = DB::table('rast')
         ->select(DB::raw('month.name as mes, variable.name as variable, avg(ST_Value(rast, ST_SetSRID(ST_Point(-71.233333,-34.983333), 4326))) as promedio'))
         ->join('register', 'register.id', '=', 'rast.id_register')
         ->join('month', 'month.id', '=', 'register.id_month')
+        ->where('register.id_period', '=', '1')
         ->join('variable', 'variable.id', '=', 'register.id_variable')
-        ->where('register.id_period', '=', $id_periodo)
-        ->orwhere('variable.id','=', $id_variable)
         ->groupBy('month.id', 'variable.name')
-        ->orderBy('month.id')
         ->get();
         return ($tabla);
     }  
