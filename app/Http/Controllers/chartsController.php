d68b07a713e18d1861329a7bec70dc7ee14cd4ed
kvr1191 <kvr1191@gmail.com>
2016-10-26 16:04:53 -0300

    meses ordenados

--- app/Http/Controllers/chartsController.php
+++ app/Http/Controllers/chartsController.php
@@ -24,8 +24,8 @@ class chartsController extends Controller
         $periodo = Period::all();// traemos todos los periodos que existen en la bd
         $scenario = Scenario::all();
         $variable = Variable::all();
-        $datosTabla = $this->datosTabla(); //se obtienen los datos para llenar la tabla (mes, variable, promedio)
-
+        $datosTabla = $this->datosTabla(1,1); //se obtienen los datos para llenar la tabla (mes, variable, promedio)
+        //dd(count($consultaPunto));
         return view('indexGrafico')-> with('lava',$lava)->with('periodo',$periodo)->with('scenario',$scenario)->with('variable',$variable)->with('datosTabla', $datosTabla);
       
     }
@@ -48,7 +48,6 @@ class chartsController extends Controller
                         'fontSize' => 30
                     ]
                 ]);
-
         return $lava;
     }
 
@@ -84,11 +83,12 @@ class chartsController extends Controller
         ->where('register.id_period','=',$id_periodo)
         ->orwhere('variable.id','=', $id_variable)
         ->groupBy('month.id')
+        ->orderBy('month.id')
         ->get();
         return $consulta;
     }
 
-    //recibo lo que tiene los lavels //esto hay que hacerlo con ajax
+    //recibo lo que tiene los labels //esto hay que hacerlo con ajax
     public function postGrafico(Request $request)
     {
         if ( $request->ajax())
@@ -103,14 +103,15 @@ class chartsController extends Controller
         
     } 
 
-    public function datosTabla()
+    public function datosTabla($id_variable, $id_periodo)
     {
         $tabla = DB::table('rast')
         ->select(DB::raw('month.name as mes, variable.name as variable, avg(ST_Value(rast, ST_SetSRID(ST_Point(-71.233333,-34.983333), 4326))) as promedio'))
         ->join('register', 'register.id', '=', 'rast.id_register')
         ->join('month', 'month.id', '=', 'register.id_month')
-        ->where('register.id_period', '=', '1')
         ->join('variable', 'variable.id', '=', 'register.id_variable')
+        ->where('register.id_period', '=', $id_periodo)
+        ->orwhere('variable.id','=', $id_variable)
         ->groupBy('month.id', 'variable.name')
         ->orderBy('month.id')
         ->get();
