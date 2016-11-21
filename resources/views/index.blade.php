ea10caf234e0010eaf70a61701b0a9c692c577f7
yerconunez <yerconunezg@gmail.com>
2016-11-21 11:26:29 -0300

    Importar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -166,6 +166,7 @@
                                      <div class="row">
                                         <div class="col-md-6 "> 
                                             <div class="custom-input-file"><input type="file" accept=".csv"onchange='readText(this)'  size="1" class="input-file" />
+                                                <button class="btn btn-block btn-success btn-lg" id="btn-Exportar" title="Exportar" > <i class="fa fa-cloud-upload"></i> Importar</button>
                                             </div>                   
                                         </div>
                                         <div class="col-md-6 ">                 
