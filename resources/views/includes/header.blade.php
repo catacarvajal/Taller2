c55e4e85d00d9e2918c7751d281527f0aeea844e
yerconunez <yerconunezg@gmail.com>
2016-12-13 16:11:12 -0300

    Cambios

--- resources/views/includes/header.blade.php
+++
@@ -1,7 +0,0 @@
-<header class="main-header">
-    <a href="/" class="logo">
-        <span class="logo-lg"><b>S</b>istema <b>I</b>nformación</span>
-    </a>
-    <nav class="navbar navbar-static-top" role="navigation">
-    </nav>
-</header>    
